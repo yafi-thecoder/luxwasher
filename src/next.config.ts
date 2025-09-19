85356a16ff787c9634c7f9b3e68f879959a54c68
Yafiah <yafiah260@gmail.com>
2025-09-19 16:04:10 +0000

    no make it fully functional and dynamic ready to publish

--- src/next.config.ts
+++ src/next.config.ts
@@ -1,7 +1,6 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export',
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
@@ -10,7 +9,6 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
